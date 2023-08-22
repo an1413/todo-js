de38b14af213bdb2eec082096b0b0e8f90dadf15
nastar <an1413@hs.ac.kr>
2023-08-22 20:25:33 +0900

    Feat: add background.js

---
+++ js/background.js
@@ -0,0 +1,31 @@
+const bgImage = document.querySelector("#wrapper");
+
+const imageUrls = [
+  "0.jpg",
+  "1.jpg",
+  "2.jpg",
+  "3.jpg",
+  "4.jpg",
+  "5.jpg",
+  "6.jpg",
+  "7.jpg",
+  "8.jpg",
+  "9.jpg",
+  "10.jpg",
+  "11.jpg",
+]
+const random = Math.floor(Math.random() * imageUrls.length);
+
+function getRandomImage() {
+  return imageUrls[random];
+}
+
+const paintImage = () => {
+  const imgURL = getRandomImage();
+  console.log(imgURL);
+  bgImage.style.backgroundImage = `url(../images/${imgURL})`;
+  console.log(bgImage.style.backgroundImage);
+  console.log(bgImage);
+}
+paintImage(random);
+
