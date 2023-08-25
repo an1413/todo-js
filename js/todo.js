d78242264c45e6b332024004343c7df0efdc8ae2
nastar <an1413@hs.ac.kr>
2023-08-25 14:55:23 +0900

    Feat: add todo.js

---
+++ js/todo.js
@@ -0,0 +1,63 @@
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoBtn = document.querySelector("#todo-btn");
+const todoList = document.querySelector("#todo-list");
+
+const TODOS_KEY = "todos"
+
+let savedList = [];
+
+console.log(todoInput.value);
+
+function savedTodoList() {
+  return localStorage.setItem(TODOS_KEY, JSON.stringify(savedList));
+}
+
+function deleteToDo(e) {
+  const li = e.target.parentElement;
+  savedList = savedList.filter((toDo) => toDo.id !== parseInt(li.id));
+  savedTodoList();
+  li.remove();
+}
+
+function createTodoList(todo) {
+  const li = document.createElement("li");
+  const text = document.createElement("span");
+  text.innerText = todo.text;
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerText = "X";
+  deleteBtn.addEventListener("click", deleteToDo)
+  li.appendChild(text);
+  li.appendChild(deleteBtn);
+  todoList.appendChild(li);
+}
+
+function handleTodoInput(e) {
+  e.preventDefault();
+  const newTodo = todoInput.value;
+  todoInput.value = "";
+  const newTodoObj = {
+    text:newTodo,
+    id: Date.now(),
+  }
+  savedList.push(newTodoObj);
+  createTodoList(newTodoObj);
+  savedTodoList();
+}
+
+todoForm.addEventListener("submit", handleTodoInput);
+
+const list = localStorage.getItem(TODOS_KEY);
+
+if(localStorage.getItem(TODOS_KEY)){
+  const arrayTodo = JSON.parse(list);
+  savedList = arrayTodo;
+  savedList.forEach(e => {
+    return createTodoList(e);
+  })
+  // console.log(JSON.stringify(list));
+  console.log("data ok");
+}
+else {
+  console.log("data no");
+}
