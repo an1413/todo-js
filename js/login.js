66f990510ef241847957de798b23aa574143a818
nastar <an1413@hs.ac.kr>
2023-08-25 17:00:54 +0900

    Fix: duplication day bug

--- js/login.js
+++ js/login.js
@@ -3,6 +3,8 @@ const loginInput = document.querySelector("#login-input");
 const loginBtn = document.querySelector("#login-btn");
 const welcome = document.querySelector("#welcome");
 const yourName = document.querySelector("#welcome span");
+// const day = document.querySelector("#day");
+const todoContainer = document.querySelector(".todo-container");
 
 function handleInputVaule(e) {
   e.preventDefault();
@@ -17,6 +19,9 @@ function loginCheck(myAccount) {
   if(localStorage.getItem(myAccount)) {
   loginForm.classList.add("disabled")
   welcome.classList.remove("disabled")
+  dayTime.classList.remove("disabled")
+  todoContainer.classList.remove("disabled")
+
 
   const yourname = loginInput.value;
   // 유저정보가 있다면
