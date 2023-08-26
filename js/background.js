3ce2a472989ce2c2f37a79406d221e981869e57c
nastar <an1413@hs.ac.kr>
2023-08-26 16:07:26 +0900

    Feat: Add Web Images

--- js/background.js
+++ js/background.js
@@ -1,5 +1,19 @@
 const bgImage = document.querySelector("#wrapper");
 
+const imageWebUrls = [
+  "https://ifh.cc/g/ckZK0p.jpg",
+  "https://ifh.cc/g/Sozaqb.jpg",
+  "https://ifh.cc/g/zg8LV5.jpg",
+  "https://ifh.cc/g/pLnca1.jpg",
+  "https://ifh.cc/g/7YsMny.jpg",
+  "https://ifh.cc/g/AW69fm.jpg",
+  "https://ifh.cc/g/nryM5J.jpg",
+  "https://ifh.cc/g/13qbY8.jpg",
+  "https://ifh.cc/g/4yOdPJ.jpg",
+  "https://ifh.cc/g/bA8QxH.jpg",
+  "https://ifh.cc/g/R6OJAW.jpg",
+]
+
 const imageUrls = [
   "0.jpg",
   "1.jpg",
@@ -14,18 +28,18 @@ const imageUrls = [
   "10.jpg",
   "11.jpg",
 ]
+
 const random = Math.floor(Math.random() * imageUrls.length);
 
 function getRandomImage() {
-  return imageUrls[random];
+  return imageWebUrls[random];
 }
 
 const paintImage = () => {
   const imgURL = getRandomImage();
   console.log(imgURL);
-  bgImage.style.backgroundImage = `url(../images/${imgURL})`;
+  bgImage.style.backgroundImage = `url(${imgURL})`;
   console.log(bgImage.style.backgroundImage);
   console.log(bgImage);
 }
 paintImage(random);
-
